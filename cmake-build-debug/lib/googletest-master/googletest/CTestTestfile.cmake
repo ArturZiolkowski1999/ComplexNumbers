3fa358e45ef95452c32edafe15253e9dc573226e
ziolko <aaartiii99@gmail.com>
2021-03-14 15:36:06 +0100

    first commit

---
+++ cmake-build-debug/lib/googletest-master/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/artur/cpp_projekty/complex_numbers/lib/googletest-master/googletest
+# Build directory: /home/artur/cpp_projekty/complex_numbers/cmake-build-debug/lib/googletest-master/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
